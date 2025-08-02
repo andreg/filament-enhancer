13333f199b77767a51b387a2266ad954181ef163
Andrea Gandino <andreagandino@gmail.com>
2025-08-02 18:11:05 +0200

    Update
    Signed-off-by: Andrea Gandino <andreagandino@gmail.com>

--- src/Resources/Pages/Traits/RedirectsToParent.php
+++ src/Resources/Pages/Traits/RedirectsToParent.php
@@ -2,9 +2,6 @@
 
 namespace Andreg\FilamentEnhancer\Resources\Pages\Traits;
 
-use Illuminate\Support\Facades\Request;
-use Illuminate\Support\Facades\Route;
-
 trait RedirectsToParent {
 
 	protected function getRedirectUrl(): string {
@@ -12,15 +9,9 @@ trait RedirectsToParent {
 			return $this->previousUrl;
 		}
 
-		$request         = Request::create( url()->previous(), 'GET' );
-		$route           = Route::getRoutes()->match( $request );
-		$routeParameters = $route->parameters();
-		$routeParameters = array_slice( $routeParameters, 0, -1, true );
-
 		return $this->getParentResource()::getUrl( 'edit', [
 			'record' => $this->getParentRecord(),
-			...$routeParameters,
-		] );
+		], shouldGuessMissingParameters: true );
 	}
 
 }
