9c039c7b276f66bd7e51288e6681c65ef157b709
Andrea Gandino <andreagandino@gmail.com>
2025-08-02 23:35:09 +0200

    Refactor MoneyColumn to allow dynamic division by adding divideBy method

--- src/Tables/Columns/MoneyColumn.php
+++ src/Tables/Columns/MoneyColumn.php
@@ -5,9 +5,16 @@ namespace Andreg\FilamentEnhancer\Tables\Columns;
 class MoneyColumn extends \Filament\Tables\Columns\TextColumn {
 
 	protected string $currency = 'USD';
+	protected int $divideBy    = 1;
 
 	public function currency( string $currency ): static {
 		$this->currency = $currency;
+
+		return $this;
+	}
+
+	public function divideBy( int $divideBy ): static {
+		$this->divideBy = $divideBy;
 
 		return $this;
 	}
@@ -17,7 +24,9 @@ class MoneyColumn extends \Filament\Tables\Columns\TextColumn {
 
 		$this->currency( config( 'enhancer.currency', $this->currency ) );
 		$this->isNumeric( true );
-		$this->money( $this->currency, divideBy: 100 );
+		$this->money( $this->currency, divideBy: function () {
+			return $this->divideBy;
+		} );
 	}
 
 }
