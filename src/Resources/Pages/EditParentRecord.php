4250874ebae5c1eb9f12d16e284559f185340419
Andrea Gandino <andreagandino@gmail.com>
2025-08-05 16:33:49 +0200

    .

---
+++ src/Resources/Pages/EditParentRecord.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Andreg\FilamentEnhancer\Resources\Pages;
+
+use Filament\Resources\Pages\EditRecord;
+
+class EditParentRecord extends EditRecord {
+	use Traits\DisplaysRelationsWithTabs;
+
+}
---
+++ src/Resources/Pages/Traits/DisplaysRelationsWithTabs.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Andreg\FilamentEnhancer\Resources\Pages\Traits;
+
+trait DisplaysRelationsWithTabs {
+
+	public function hasCombinedRelationManagerTabsWithContent(): bool {
+		return true;
+	}
+
+}
