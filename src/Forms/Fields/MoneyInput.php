27faf3e78edd019c4068df617d3f0e6ffcd6e31a
Andrea Gandino <andreagandino@gmail.com>
2025-08-18 21:48:03 +0200

    .

--- src/Forms/Fields/MoneyInput.php
+++ src/Forms/Fields/MoneyInput.php
@@ -2,6 +2,7 @@
 
 namespace Andreg\FilamentEnhancer\Forms\Fields;
 
+use Filament\Forms\Components\Field;
 use Filament\Forms\Components\TextInput;
 use Filament\Support\RawJs;
 
@@ -44,6 +45,10 @@ class MoneyInput extends TextInput {
 			return $formatter->format( floatval( $state ) );
 		} );
 
+		$this->afterStateUpdated( function ( $state, Field $component ) use ( $formatter ) {
+			$component->state( $formatter->format( floatval( $state ) ) );
+		} );
+
 		$this->dehydrateStateUsing( function ( $state ) use ( $formatter ) {
 			$state = str_replace( $formatter->getSymbol( \NumberFormatter::GROUPING_SEPARATOR_SYMBOL ), '', $state );
 			$state = str_replace( $formatter->getSymbol( \NumberFormatter::DECIMAL_SEPARATOR_SYMBOL ), '.', $state );
@@ -51,18 +56,18 @@ class MoneyInput extends TextInput {
 			return (float) $state;
 		} );
 
-		$this->mask( function () use ( $formatter ) {
-			return RawJs::make(
-				strtr(
-					'$money($input, \'{decimalSeparator}\', \'{groupingSeparator}\', {fractionDigits})',
-					[
-						'{decimalSeparator}'  => $formatter->getSymbol( \NumberFormatter::DECIMAL_SEPARATOR_SYMBOL ),
-						'{groupingSeparator}' => $formatter->getSymbol( \NumberFormatter::GROUPING_SEPARATOR_SYMBOL ),
-						'{fractionDigits}'    => $formatter->getAttribute( \NumberFormatter::FRACTION_DIGITS ),
-					]
-				)
-			);
-		} );
+		// $this->mask( function () use ( $formatter ) {
+		// 	return RawJs::make(
+		// 		strtr(
+		// 			'$money($input, \'{decimalSeparator}\', \'{groupingSeparator}\', {fractionDigits})',
+		// 			[
+		// 				'{decimalSeparator}'  => $formatter->getSymbol( \NumberFormatter::DECIMAL_SEPARATOR_SYMBOL ),
+		// 				'{groupingSeparator}' => $formatter->getSymbol( \NumberFormatter::GROUPING_SEPARATOR_SYMBOL ),
+		// 				'{fractionDigits}'    => $formatter->getAttribute( \NumberFormatter::FRACTION_DIGITS ),
+		// 			]
+		// 		)
+		// 	);
+		// } );
 	}
 
 }
